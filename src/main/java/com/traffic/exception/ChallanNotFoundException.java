fb247b39ca0c9ca7e7278619e7a657c090920661
nandanaj2006 <nandanaj2103@gmail.com>
2026-09-10 15:02:56 +0530

    Add ChallanNotFoundException class

--- src/main/java/com/traffic/exception/ChallanNotFoundException.java
+++ src/main/java/com/traffic/exception/ChallanNotFoundException.java
@@ -1,1 +1,7 @@
-aa
+package com.traffic.exception;
+
+public class ChallanNotFoundException extends RuntimeException {
+    public ChallanNotFoundException(String message) {
+        super(message);
+    }
+}
