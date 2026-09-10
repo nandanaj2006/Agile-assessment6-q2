1a160d9827114ef006b82a1d346e0e3f3d90034d
nandanaj2006 <nandanaj2103@gmail.com>
2026-09-10 15:01:31 +0530

    Update Vehicle.java

--- src/main/java/com/traffic/model/Vehicle.java
+++ src/main/java/com/traffic/model/Vehicle.java
@@ -1,1 +1,27 @@
-aa
+package com.traffic.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Vehicle {
+    private String vehicleNumber;
+    private String ownerName;
+    private VehicleType vehicleType;
+    private List<Challan> violationHistory;
+
+    public Vehicle(String vehicleNumber, String ownerName, VehicleType vehicleType) {
+        this.vehicleNumber = vehicleNumber;
+        this.ownerName = ownerName;
+        this.vehicleType = vehicleType;
+        this.violationHistory = new ArrayList<>();
+    }
+
+    public String getVehicleNumber() { return vehicleNumber; }
+    public String getOwnerName() { return ownerName; }
+    public VehicleType getVehicleType() { return vehicleType; }
+    public List<Challan> getViolationHistory() { return violationHistory; }
+    
+    public void addViolationToHistory(Challan challan) { 
+        this.violationHistory.add(challan); 
+    }
+}
